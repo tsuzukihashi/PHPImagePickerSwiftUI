fd89f698b94fbfb9f14d8283c68df0f3e57c4aff
tsuzukihashi <tsudukihashi0817@gmail.com>
2022-02-01 19:42:37 +0900

    prot

--- Sources/PHPImagePickerSwiftUI/PHPImagePickerSwiftUI.swift
+++ Sources/PHPImagePickerSwiftUI/PHPImagePickerSwiftUI.swift
@@ -1,6 +1,56 @@
-public struct PHPImagePickerSwiftUI {
-    public private(set) var text = "Hello, World!"
+import PhotosUI
+import SwiftUI
 
-    public init() {
+@available(iOS 14, *)
+public struct PHPImagePickerSwiftUI: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+
+    var configuration: PHPickerConfiguration
+
+    public init(
+        configuration: PHPickerConfiguration,
+        selectedImag: Binding<UIImage?>
+    ) {
+        self.configuration = configuration
+        self._selectedImage = selectedImag
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    public func makeUIViewController(context: Context) -> PHPickerViewController {
+        let vc = PHPickerViewController(configuration: configuration)
+        vc.delegate = context.coordinator
+        return vc
+    }
+
+    public func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) { }
+
+    public class Coordinator: PHPickerViewControllerDelegate {
+        let parent: PHPImagePickerSwiftUI
+
+        init(_ parent: PHPImagePickerSwiftUI) {
+            self.parent = parent
+        }
+
+        public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            for result in results {
+                let itemProvider = result.itemProvider
+
+                if itemProvider.canLoadObject(ofClass: UIImage.self) {
+                    itemProvider.loadObject(ofClass: UIImage.self) { item, error in
+                        if let image = item as? UIImage {
+                            self.parent.selectedImage = image
+                        }
+
+                        if let error = error {
+                            print(error.localizedDescription)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
